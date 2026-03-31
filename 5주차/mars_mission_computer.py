ef4920169d36c299921d1ebfe7db67174b449c47
seoyoung000 <128365055+seoyoung000@users.noreply.github.com>
2026-03-31 19:37:26 +0900

    Refactor DummySensor to use private variable

--- 5주차/mars_mission_computer.py
+++ 5주차/mars_mission_computer.py
@@ -5,7 +5,7 @@ class DummySensor:
 
     def __init__(self):
         # 환경 데이터를 저장할 사전(dict) 객체 초기화
-        self.env_values = {
+        self._env_values = {
             "mars_base_internal_temperature": 0.0,
             "mars_base_external_temperature": 0.0,
             "mars_base_internal_humidity": 0.0,
@@ -15,32 +15,26 @@ class DummySensor:
         }
 
     def set_env(self):
-        """
-        각 환경 항목에 대해 지정된 범위 내의 랜덤 값을 생성하여 저장함.
-        """
         # 내부 온도: 18~30도
-        self.env_values["mars_base_internal_temperature"] = random.uniform(18, 30)
+        self._env_values["mars_base_internal_temperature"] = random.uniform(18, 30)
         
         # 외부 온도: 0~21도
-        self.env_values["mars_base_external_temperature"] = random.uniform(0, 21)
+        self._env_values["mars_base_external_temperature"] = random.uniform(0, 21)
         
         # 내부 습도: 50~60%
-        self.env_values["mars_base_internal_humidity"] = random.uniform(50, 60)
+        self._env_values["mars_base_internal_humidity"] = random.uniform(50, 60)
         
         # 외부 광량: 500~715 W/m2
-        self.env_values["mars_base_external_illuminance"] = random.uniform(500, 715)
+        self._env_values["mars_base_external_illuminance"] = random.uniform(500, 715)
         
         # 내부 이산화탄소 농도: 0.02~0.1%
-        self.env_values["mars_base_internal_co2"] = random.uniform(0.02, 0.1)
+        self._env_values["mars_base_internal_co2"] = random.uniform(0.02, 0.1)
         
         # 내부 산소 농도: 4%~7%
-        self.env_values["mars_base_internal_oxygen"] = random.uniform(4, 7)
+        self._env_values["mars_base_internal_oxygen"] = random.uniform(4, 7)
 
     def get_env(self):
-        """
-        현재 저장된 환경 데이터 사전 객체를 반환함.
-        """
-        return self.env_values
+        return self._env_values
 
 
 if __name__ == "__main__":
